a9eaab2f378c423d4e846e298897e022171c963a
Scott Givan <sgivan@yahoo.com>
2016-08-20 23:57:56 -0500

    initial commit

---
+++ cmd
@@ -0,0 +1,2 @@
+rsync -avz -e ssh scott@192.168.1.9:'/Users/scott/Music/iTunes/iTunes\ Media/Music' ./
+rsync -avh -e ssh genomeogenesis.com:/mnt/home/givans/Music ./
---
+++ sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd $HOME
+#rsync -avh -e ssh genomeogenesis.com:/mnt/home/givans/Music ./
+rsync -avh -e "ssh -p 743" givans@192.168.1.18:/mnt/home/givans/Music ./
