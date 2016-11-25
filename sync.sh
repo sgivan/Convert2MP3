bbbbe14f99e6cd21316398341ee176b7c3e12281
Scott Givan <sgivan@yahoo.com>
2016-11-25 15:14:33 -0600

    Works to rsync from hammer to roy.

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@
 cd $HOME
 #rsync -avh -e ssh genomeogenesis.com:/mnt/home/givans/Music ./
 #rsync -avh -e "ssh -p 743" givans@192.168.1.18:/mnt/home/givans/Music ./
-rsync -avh -e ssh sgivan@hammer:/home/sgivan/Music ./
+rsync -avh -e ssh sgivan@10.0.0.24:Music ./
