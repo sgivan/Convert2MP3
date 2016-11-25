fcea9eaae8e6dec6863cae426743d1be4f3e8217
Scott Givan <sgivan@yahoo.com>
2016-11-25 14:49:16 -0600

    trying to debug sync.sh to use hammer

--- sync.sh
+++ sync.sh
@@ -2,4 +2,5 @@
 
 cd $HOME
 #rsync -avh -e ssh genomeogenesis.com:/mnt/home/givans/Music ./
-rsync -avh -e "ssh -p 743" givans@192.168.1.18:/mnt/home/givans/Music ./
+#rsync -avh -e "ssh -p 743" givans@192.168.1.18:/mnt/home/givans/Music ./
+rsync -avh -e ssh sgivan@hammer:/home/sgivan/Music ./
