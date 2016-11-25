fd751e4bca85d9b3cedfe95ec5282bb8d0d1ee0f
Scott Givan <sgivan@yahoo.com>
2016-11-25 16:27:03 -0600

    generates recursive list of files

--- convert_to_mp3.py
+++ convert_to_mp3.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 
-from os import listdir
-from os.path import isfile, join
+from os import listdir, walk
+import os.path
+import string
 
-for f in listdir("./"):
-    print(f)
+f = []
+alldirpaths = []
+alldirnames = []
+allfilenames = []
 
+cnt = 0
+for (dirpath, dirnames, filenames) in os.walk("./"):
+    print dirpath
+    for f in filenames:
+        allfilenames.append(os.path.join(dirpath, f))
 
+for fname in allfilenames:
+    print fname
+
+#for dname in alldirnames:
+#    print(dname)
+
+#for dpath in alldirpaths:
+#    print("'" + dpath + "'")
+    
