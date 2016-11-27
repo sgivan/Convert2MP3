a3fbd55d49939c089869bd52441a1b28e373a84b
Scott Givan <sgivan@yahoo.com>
2016-11-26 22:55:25 -0600

    convert_to_mp3.py works. Still adding features, though.

--- convert_to_mp3.py
+++ convert_to_mp3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import sys, argparse
 import os
 import os.path
 import re
@@ -12,15 +12,22 @@ alldirpaths = []
 alldirnames = []
 allfilenames = []
 
+# parser command line options
+parser = argparse.ArgumentParser()
+parser.add_argument("--limit", help="limit number of files processed", default=30)
+args = parser.parse_args()
+
 cnt1 = 0
 cnt2 = 0
+of = open("warnings.txt", "w")
+
 cnv_script = os.environ['HOME'] + "/projects/audprocess/bin/convert_m4a_to_hq-mp3.pl"
 print cnv_script
 
 for (dirpath, dirnames, filenames) in os.walk("./"):
 
     for f in filenames:
-        if (cnt2 > 3):
+        if (cnt2 > args.limit):
             break
         matchobj = re.match( r'(.+)m4a$', f, re.M|re.I)
 
@@ -32,11 +39,18 @@ for (dirpath, dirnames, filenames) in os.walk("./"):
                 print os.path.join(dirpath,newfile) + " exists"
             else:
                 print os.path.join(dirpath,newfile) + " does not exist"
-                rtn = subprocess.call(cnv_script + " --infile '" + os.path.  join(dirpath, f) + "' -b320", shell=True);
+                cmd = cnv_script + " --infile " + re.escape(os.path.join(dirpath, f)) + " -b320 --debug"
+                print "cmd: " + cmd
+                #rtn = subprocess.call(cnv_script + " --infile \"" + os.path.join(dirpath, f) + "\" -b320 --debug", shell=True);
+                rtn = subprocess.call(cmd, shell=True);
                 cnt2 += 1
                 if (rtn):
+                    of.write(os.path.join(dirpath, f))
                     sys.exit("abnormal return from conversion script: '" + str(rtn) +"'")
 
+
+
+of.close();
 
 #        if (matchobj):
 #            allfilenames.append(os.path.join(dirpath, f))
