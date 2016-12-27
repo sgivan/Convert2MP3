954c864280526284c289a475ed33f8fc0f52b45c
Scott Givan <sgivan@yahoo.com>
2016-12-27 11:40:54 -0600

    This version of convert_to_mp3.py works as expected.

--- convert_to_mp3.py
+++ convert_to_mp3.py
@@ -15,7 +15,25 @@ allfilenames = []
 # parser command line options
 parser = argparse.ArgumentParser()
 parser.add_argument("--limit", help="limit number of files processed", default=30)
+parser.add_argument("--bitrate", help="bitrate of converted files; pick 128, 256 or 320", default=320)
 args = parser.parse_args()
+
+looplimit = args.limit
+bitrate = 320
+bitrateflag = "--b320"
+if (args.bitrate):
+        if (args.bitrate == '128'):
+            bitrateflag = "--b128"
+        elif (args.bitrate == '256'):
+            bitrateflag = "--b256"
+'''
+if (1):
+    print "args.bitrate " + args.bitrate
+    print "args.limit " + args.limit
+    if (args.bitrate == '256'):
+        print "yes, " + args.bitrate
+    sys.exit()
+'''
 
 cnt1 = 0
 cnt2 = 0
@@ -27,8 +45,10 @@ print cnv_script
 for (dirpath, dirnames, filenames) in os.walk("./"):
 
     for f in filenames:
-        if (cnt2 > args.limit):
+
+        if (cnt2 >= int(args.limit)):
             break
+
         matchobj = re.match( r'(.+)m4a$', f, re.M|re.I)
 
         if (matchobj):
@@ -39,7 +59,8 @@ for (dirpath, dirnames, filenames) in os.walk("./"):
                 print os.path.join(dirpath,newfile) + " exists"
             else:
                 print os.path.join(dirpath,newfile) + " does not exist"
-                cmd = cnv_script + " --infile " + re.escape(os.path.join(dirpath, f)) + " -b320"
+                #cmd = cnv_script + " --infile " + re.escape(os.path.join(dirpath, f)) + " --b320"
+                cmd = cnv_script + " --lowerbitrate --infile " + re.escape(os.path.join(dirpath, f)) + " " + bitrateflag
                 print "cmd: " + cmd
                 #rtn = subprocess.call(cnv_script + " --infile \"" + os.path.join(dirpath, f) + "\" -b320", shell=True);
                 rtn = subprocess.call(cmd, shell=True);
